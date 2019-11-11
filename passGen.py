03ca0cef998b3ec1a0d1f418e598a86febeee667
Jay-Goss <42222447+Jay-Goss@users.noreply.github.com>
2019-11-10 20:34:08 -0500

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -16,13 +16,36 @@ random.choice(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '='])
 # print(random.choice(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=']))
 
 def pwtype():
-    pwtype.Type = input("Create your password. Type 1 for alphanumeric and 2 for alphanumeric with symbols.")
+    pwtype.Type = input("Create your password. Type 1 for alphanumeric only and 2 to include symbols.")
 def pwlength():
     pwlength.Length = input("Choose the character length.")
-type()
-# print(pwType)
-# print(pwLength)
-if pwtype.Type == "1":
-    print("yes1")
-else pwtype.Type == "2":
-    print("yes2")
+pwtype()
+pwlength()
+# print(pwlength.Length)
+if pwtype.Type == str(1):
+    i=0
+    pw = ''
+    while (i < int(pwlength.Length)):
+        x = random.randint(0,1)
+        if x == int(0):
+            pw = pw + str(random.choice(string.ascii_letters))
+            i+=1
+        elif x == int(1):
+            pw = pw + str(random.randint(0,9))
+            i+=1
+    print(str(pw))
+elif pwtype.Type == str(2):
+    i=0
+    pw = ''
+    while (i < int(pwlength.Length)):
+        x = random.randint(0,2)
+        if x == int(0):
+            pw = pw + str(random.choice(string.ascii_letters))
+            i+=1
+        elif x == int(1):
+            pw = pw + str(random.choice(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=']))
+            i+=1
+        elif x == int(1):
+            pw = pw + str(random.randint(0,9))
+            i+=1
+    print("Here is your randomly generated password: " + str(pw))
